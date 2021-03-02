ce3e44dff8a29a8e31672de5975aef310b72cf2f
briankro951 <56939484+briankro951@users.noreply.github.com>
2021-03-01 21:22:42 -0800

    2021 03 01

--- stuff.js
+++ stuff.js
@@ -2,7 +2,7 @@ $(document).ready(mainCode)
 
 function toggleMute() {
 
-    var video = document.getElementById("myVideo")
+    var video = document.getElementById("video1")
 
     if (video.muted) {
         video.muted = false;
